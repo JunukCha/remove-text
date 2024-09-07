1aa644b6b550e0e94df7754f1e6033016cb9fc0a
Junuk Cha <jucha@unist.ac.kr>
2024-09-07 16:28:59 +0200

    inpaint

--- scripts/run_inpaint.sh
+++ scripts/run_inpaint.sh
@@ -1,1 +1,1 @@
-python test.py --video data/video_1_with_subtitles_LT2RB.mp4 --mask data/video_1_with_subtitles_LT2RB  --ckpt checkpoints/sttn.pth 
+python main_inpaint.py --video data/video_1_with_subtitles_LT2RB.mp4 --mask data/video_1_with_subtitles_LT2RB  --ckpt checkpoints/sttn.pth 
